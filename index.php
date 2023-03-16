46411a75821ebea2a3cd313e3f6a81175c3171e4
sakibsarker <sakib15-3824@diu.edu.bd>
2023-03-17 03:06:33 +0600

    header and footer added

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Innovation</title>
-</head>
-<body>
-    Hello
-</body>
-</html>
-
 <?php
+include_once 'header.php';
 ?>
+<div class="content">
+    <h3> Welcome to Custom THEME</h3>
+</div>
+
+
+<?php include_once 'footer.php'; ?>
